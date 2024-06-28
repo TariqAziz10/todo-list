523e6a115b83daf01b20ef20f83c6cac27d5a40d
TariqAziz10 <rockersss250@gmail.com>
2024-06-28 16:54:10 +0530

    js Ist final

---
+++ todo.js
@@ -0,0 +1,17 @@
+let inputs = document.getElementById("inp");
+
+let text = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value == ""){
+        alert("Please Enter Tasks")
+    }else{
+        let newE = document.createElement("ul");
+        newE.innerHTML = `${inputs.value} <i class="fa-solid fa-trash-can"></i>`;
+        text.appendChild(newE);
+        inputs.value = "";
+        newE.querySelector("i").addEventListener("click" , function(){
+            newE.remove()
+        });
+    }
+}
