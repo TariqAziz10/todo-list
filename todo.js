afc22d90ca2612e4f35ecfa96addc0c8924d32cd
TariqAziz10 <rockersss250@gmail.com>
2024-06-29 01:00:39 +0530

    j

--- todo.js
+++ todo.js
@@ -10,8 +10,18 @@ function Add(){
         newE.innerHTML = `${inputs.value} <i class="fa-solid fa-trash-can"></i>`;
         text.appendChild(newE);
         inputs.value = "";
+        
         newE.querySelector("i").addEventListener("click" , function(){
             newE.remove()
+            
         });
+        
     }
 }
+function saveData(){
+    localStorage.setItem("data", newE.innerHTML);
+}
+function showTask(){
+    newE.innerHTML = localStorage.getItem("data");
+}
+showTask();
